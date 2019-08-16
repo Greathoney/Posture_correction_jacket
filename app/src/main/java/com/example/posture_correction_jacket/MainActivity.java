a1cab69a3a49bcaf92195e68ec0ee6df251d5c31
Greathoney <44860482+Greathoney@users.noreply.github.com>
2019-08-16 14:12:40 +0900

    Commit

--- app/src/main/java/com/example/posture_correction_jacket/MainActivity.java
+++ app/src/main/java/com/example/posture_correction_jacket/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.posture_correction_jacket;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.posture_correction_jacket.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button pairing_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        pairing_button = findViewById(R.id.pairing_button);
+
+        pairing_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, Bluetooth_pairingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
