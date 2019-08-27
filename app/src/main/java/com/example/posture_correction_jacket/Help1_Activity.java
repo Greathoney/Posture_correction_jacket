ab8828828b10aed00563208fd97ad4dd89122250
Greathoney <thyoon48@gmail.com>
2019-08-27 18:17:27 +0900

    8/27 여러가지 커밋함

--- app/src/main/java/com/example/posture_correction_jacket/Help1_Activity.java
+++ app/src/main/java/com/example/posture_correction_jacket/Help1_Activity.java
@@ -1,9 +1,15 @@
 package com.example.posture_correction_jacket;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Help1_Activity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help1);
 
-
+    }
 }
