b7c3d496632f963e2ef174d81f9ac85a6926e190
Greathoney <thyoon48@gmail.com>
2019-08-27 21:27:36 +0900

    commited

--- app/src/main/java/com/example/posture_correction_jacket/Help1_Activity.java
+++ app/src/main/java/com/example/posture_correction_jacket/Help1_Activity.java
@@ -1,6 +1,9 @@
 package com.example.posture_correction_jacket;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
